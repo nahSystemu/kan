b8d1fe230d43a29226bddd5ac6c73dd832fa5123
Henry <henry_ball@hotmail.co.uk>
2026-03-13 23:08:46 +0000

    fix: prevent empty string from throwing error

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,7 +1,7 @@
 import pino from "pino";
 
 const isDev = process.env.NODE_ENV !== "production";
-const level = process.env.LOG_LEVEL ?? (isDev ? "debug" : "info");
+const level = process.env.LOG_LEVEL || (isDev ? "debug" : "info");
 
 export const logger = pino({
   level,
